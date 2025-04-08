453dd741668931332b7edb420c315bc7b62b4059
23Chandan <chandan.in63@gmail.com>
2025-04-09 00:02:30 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HavenClubAndResort.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
